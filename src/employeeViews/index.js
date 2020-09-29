375ee878c2699c3c9b9f2abb47b3c14abd83f269
eilianacar <eilianacar@gmail.com>
2020-09-28 23:58:50 -0300

    Exportación de componentes de las capas LeaderViews y EmployeeViews

---
+++ src/employeeViews/index.js
@@ -0,0 +1,7 @@
+// SE RECOMIENDA QUE ESTA CAPA EMPLOYEEVIEWS SE MUEVA A LA CAPA VIEWS/EMPLOYEE 
+export * from './EFeedBack';
+export * from './EMap';
+export * from './EProfile';
+export * from './ESingle';
+export * from './EStars';
+export * from './ETeam';
---
+++ src/leaderViews/index.js
@@ -0,0 +1,8 @@
+// SE RECOMIENDA QUE ESTA CAPA LEADERVIEWS SE MUEVA A LA CAPA VIEWS/LEADER 
+export * from "./FeedBack";
+export * from './MapLeader';
+export * from './Profile';
+export * from './SingleLeader';
+export * from './Task';
+export * from './Team';
+export * from './Winners';
